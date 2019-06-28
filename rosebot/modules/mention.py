8134512dc7a9e5178924917e240fd561b1db2404
Sander Viirmaa <h4n1virus@gmail.com>
2019-06-29 01:50:17 +0300

    Update mention.py

--- rosebot/modules/mention.py
+++ rosebot/modules/mention.py
@@ -6,6 +6,6 @@ from rosebot.helpers import ReplyCheck
 @BOT.on_message(Filters.me & Filters.command('m', '.'))
 def mention_everyone(bot: BOT, message: Message):
     BOT.send_message(message.chat.id,
-    str(message.command) + "".join("[\u200E](tg://user?id={})".format(x.user.id) for x in BOT.iter_chat_members(message.chat.id)),
+    message.text.replace('.m', '') + "".join("[\u200E](tg://user?id={})".format(x.user.id) for x in BOT.iter_chat_members(message.chat.id)),
     reply_to_message_id=ReplyCheck(message))
     message.delete()
